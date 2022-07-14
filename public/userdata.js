f59888c7edcb0466ccf41c951cc6191c3399337d
Spytechx <105213848+Spytechx@users.noreply.github.com>
2022-07-14 14:00:59 +0100

    Public
    Public Folder 1

---
+++ public/userdata.js
@@ -0,0 +1,22 @@
+export const portfolioData = [
+  {
+    id: 1,
+    text: "Dive into the world of NFTs",
+    contents:
+      " Open rare Mystery Boxes, explore IGOs, Fan Tokens, and more with Binance NFT.",
+    img_src: "../Images/pix4.png",
+  },
+  {
+    id: 2,
+    text: "Grow your business with Binance Pay",
+    contents:
+      " Reach more customers as you pay and get paid in crypto with our borderless payment technology on Binance Pay & Binance Marketplace.",
+    img_src: "../Images/pix3.png",
+  },
+  {
+    id: 3,
+    text: "Binance Earn",
+    contents: "Hold your crypto funds and start earning.",
+    img_src: "../Images/pix2.png",
+  },
+];
